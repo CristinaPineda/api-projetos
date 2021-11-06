7e51390f943521fedc0626d7e8c811e13ffb202c
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-11-06 12:08:30 -0300

    configura handlebars

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+const hbs = exphbs.create();
+const PORT_URL = 5000;
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.get("/", (_req, res) => {
+  const name = "Projeto API";
+  res.render("home", { name });
+});
+
+app.listen(PORT_URL, () => {
+  console.log(`listening on port ${PORT_URL}`);
+});
