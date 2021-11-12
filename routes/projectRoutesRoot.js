25b2a909abaae60594180d66a1aea533ecc9b29d
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-11-12 19:26:30 -0300

    index diff

--- routes/projectRoutesRoot.js
+++ routes/projectRoutesRoot.js
@@ -1,25 +1,25 @@
-require("dotenv").config();
-const routerRoot = require("express").Router();
+// require("dotenv").config();
+// const routerRoot = require("express").Router();
 
-const ADMIN = process.env.ADMIN;
-const LOG = process.env.LOG;
+// const ADMIN = process.env.ADMIN;
+// const LOG = process.env.LOG;
 
-routerRoot.get("/", (_req, res) => {
-    const name = "API projetos pessoais";
-    res.render("home", { name });
-    return
-});
+// routerRoot.get("/", (_req, res) => {
+//     const name = "API projetos pessoais";
+//     res.render("home", { name });
+//     return
+// });
 
-routerRoot.post("/", (req, res) => {
-  if (req.body.id == ADMIN && req.body.pass == LOG) {
-    req.session.id = ADMIN;
-    const system = "API Projetos de portifólio"
-    res.render("admin", { system })
-  } else {
-    const name = "É necessário ter autorização para entrar!";
-    res.render("home", { name })
-  }
-  return
-})
+// routerRoot.post("/", (req, res) => {
+//   if (req.body.id == ADMIN && req.body.pass == LOG) {
+//     req.session.id = ADMIN;
+//     const system = "API Projetos de portifólio"
+//     res.render("admin", { system })
+//   } else {
+//     const name = "É necessário ter autorização para entrar!";
+//     res.render("home", { name })
+//   }
+//   return
+// })
 
-module.exports = routerRoot;
+// module.exports = routerRoot;
