96b97df2bf2fef1d1448553386be0ec7a14b8d99
CristinaPineda <cristinadaspineda@yahoo.com.br>
2022-03-01 15:49:35 -0300

    [refactor] :recycle: muda rota

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(cors());
 app.use('/portfolio', projectRoutes);
 
 app.listen(PORT_URL, () => {
-  console.log('Servidor rodando');
+  console.log(`Servidor rodando na porta ${PORT_URL}`);
 });
 
 export default app;
